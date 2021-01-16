20cbcef99d9a9e6c32fa15be41c80574e22d5d12
haneen <haneen.khasawneh96@gmail.com>
2021-01-16 02:16:45 +0200

    lab09

--- JS/app.js
+++ JS/app.js
@@ -187,7 +187,7 @@ function submitter(event) {
 
   var AddLocation = new SalmonCookie(LocName, MinCustomers, MaxCustomers, AvgCookies);
 
-  //SalmonCookieTable.removeChild(TotalRow);
+  SalmonCookieTable.removeChild(TotalRow);
   AddLocation.getRandomCustPerHour();
   AddLocation.getAmountOfCookies();
   AddLocation.Render();
@@ -196,13 +196,13 @@ function submitter(event) {
   
 
 
- /* for (var i = 0; i < TotalRow.childNodes.length -1; i++) {
+  for (var i = 0; i < TotalRow.childNodes.length -1; i++) {
     TotalRow.childNodes[i].textContent = TotalOfSingleHour[i -1];
     
   }
 
   TotalRow.childNodes[i].textContent = grandTotal;
-  SalmonCookieTable.appendChild(TotalRow);*/
+  SalmonCookieTable.appendChild(TotalRow);
   
  
 }
